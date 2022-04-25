a9f9e8736082c90cff5f194a303a448b4311ea19
theactofshining <ruslan.e.valikhanov@gmail.com>
2022-04-25 15:12:16 +0200

    set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+const contentful = require("contentful");
+const client = contentful.createClient({
+  host: "preview.contentful.com",
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: "pswo94mknuin",
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: "4aNCbkAhlMxIboRs-rB6e0w5MgkS2H4h-D7WMffuns4"
+});
+// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
+client
+  .getEntry("5PeGS2SoZGSa4GuiQsigQu")
+  .then(entry => console.log(entry))
+  .catch(err => console.log(err));
+
+
 function App() {
   return (
     <div className="App">
