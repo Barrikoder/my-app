ac275f56b71158357abf9d209551ef05b38458bb
anton litvak <anton.k.litvak@gmail.com>
2022-04-26 18:20:46 +0200

    got header yeyyy

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 const contentful = require("contentful");
 const client = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
   space: "pswo94mknuin",
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: "4aNCbkAhlMxIboRs-rB6e0w5MgkS2H4h-D7WMffuns4"
+  accessToken: "4aNCbkAhlMxIboRs-rB6e0w5MgkS2H4h-D7WMffuns4",
 });
 
 //put fetch data inside use effect
 
-  // const fetchData = async() => { //is a function with await inside of it
-  //   const response = await (client.getEntries()) //function provided by contentful. they will call url for me. one can use await only inside the function
-  //   // return response.items
-  //   console.log(response.items)
-  // }
+// const fetchData = async() => { //is a function with await inside of it
+//   const response = await (client.getEntries()) //function provided by contentful. they will call url for me. one can use await only inside the function
+//   // return response.items
+//   console.log(response.items)
+// }
 
-  // const fetchData = () =>
-  // fetch(client)
-  //   .then((res) => res.json())
-  //   .then(data => data.text)
-  //   .then((text) => )
-    // .then((json) => setText(json.description));
+// const fetchData = () =>
+// fetch(client)
+//   .then((res) => res.json())
+//   .then(data => data.text)
+//   .then((text) => )
+// .then((json) => setText(json.description));
 
 function App() {
-  const [text, setText] = useState("Here we go")
+  const [text, setText] = useState("Here we go");
 
   const fetchData = () => {
-    client.getEntries() //right function which takes the object - look documentation. 
-      .then((entries) => console.log(entries))
-      .catch(console.error)
-    }
-    
+    client
+      .getEntries() //right function which takes the object - look documentation.
+      .then((entries) => {
+        setText(entries.items[0].fields.header);
+        console.log(entries);
+      })
+      .catch(console.error);
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
 
-  return (
-    <h1>Hello!</h1>
-  );
+  return <h1>{text}</h1>;
 }
 
 export default App;
