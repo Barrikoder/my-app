16834bd44829fb4a9d0a54f6cadb7e609c500ca2
theactofshining <ruslan.e.valikhanov@gmail.com>
2022-04-26 16:32:44 +0200

    merged with Xiang

--- src/App.js
+++ src/App.js
@@ -1,41 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 const contentful = require("contentful");
 const client = contentful.createClient({
-  host: "cdn.contentful.com",
   // This is the space ID. A space is like a project folder in Contentful terms
   space: "pswo94mknuin",
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
   accessToken: "4aNCbkAhlMxIboRs-rB6e0w5MgkS2H4h-D7WMffuns4"
 });
 
-// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access //token.
-//client//
-//  .getEntry("5PeGS2SoZGSa4GuiQsigQu")//
-//  .then(entry => console.log(entry))//
-//  .catch(err => console.log(err));
-client.getContentType('cookbookBlog')
-.then((contentType) => console.log(contentType))
-.catch(console.error)
- 
+client.getEntries('cookbookBlog') //right function which takes the object - look documentation. 
+  .then((contentType) => console.log(contentType))
+  .catch(console.error)
+
+  const fetchData = async() => { //is a function with await inside of it
+    const response = await (client.getEntries()) //function provided by contentful. they will call url for me. one can use await only inside the function
+    // return response.items
+    console.log(response.items)
+  }
+
+  fetchData()
+  .then()
+  
+
+  // const fetchData = () =>
+  // fetch(client)
+  //   .then((res) => res.json())
+  //   .then(data => data.text)
+  //   .then((text) => )
+    // .then((json) => setText(json.description));
+
+
+const Description = () => {
+  const [text, setText] = useState("Here we go")
+  useEffect(() => {
+    fetchData(setText);
+  }, []);
+  return <button onClick={() => Description()}>Text</button>
+
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Description />
     </div>
   );
 }
