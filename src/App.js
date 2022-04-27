9ad3adc2a942063a2917848cbfe94f4b52a3b967
theactofshining <ruslan.e.valikhanov@gmail.com>
2022-04-27 18:38:20 +0200

    Merge branch 'brkkk' of https://github.com/Barrikoder/my-app into brkkk

--- src/App.js
+++ src/App.js
@@ -1,51 +1,34 @@
-
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import Description from './Description';
+import MediaCard from "./Components/MediaCard";
+import ImageCard from "./Components/ImageCard";
 
 
-const contentful = require("contentful");
-const client = contentful.createClient({
-  // This is the space ID. A space is like a project folder in Contentful terms
-  space: "pswo94mknuin",
-  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: "4aNCbkAhlMxIboRs-rB6e0w5MgkS2H4h-D7WMffuns4",
-});
-
-//put fetch data inside use effect
-
-// const fetchData = async() => { //is a function with await inside of it
-//   const response = await (client.getEntries()) //function provided by contentful. they will call url for me. one can use await only inside the function
-//   // return response.items
-//   console.log(response.items)
-// }
-
-// const fetchData = () =>
-// fetch(client)
-//   .then((res) => res.json())
-//   .then(data => data.text)
-//   .then((text) => )
-// .then((json) => setText(json.description));
 
 function App() {
-  const [text, setText] = useState("Here we go");
+  // const [text, setText] = useState("Here we go");
 
-  const fetchData = () => {
-    client
-      .getEntries() //right function which takes the object - look documentation.
-      .then((entries) => {
-        setText(entries.items[0].fields.header);
-        console.log(entries);
-      })
-      .catch(console.error);
-  };
+  // const fetchData = () => {
+  //   client
+  //     .getEntries() //right function which takes the object - look documentation.
+  //     .then((entries) => {
+  //       setText(entries.items[0].fields.header);
+  //       console.log(entries);
+  //     })
+  //     .catch(console.error);
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
-  return <h1>{text}</h1>;
+  return (
+  <>
+    <div className="card">
+    <MediaCard />
+    </div>
+  </>
+  );
 
 }
 
---
+++ src/Components/MediaCard.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography'
+import ImageCard from "./ImageCard"
+
+const contentful = require("contentful");
+const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: "pswo94mknuin",
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: "4aNCbkAhlMxIboRs-rB6e0w5MgkS2H4h-D7WMffuns4",
+});
+
+export default function MediaCard() {
+    const [data, setData] = useState([]);
+    const fetchData = () => {
+        client
+            .getEntries() //right function which takes the object - look documentation.
+            .then((entries) => {
+                setData(entries.items);
+                // console.log(entries);
+            })
+            .catch(console.error);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div>{data && data.map((entry) => {
+            console.log(entry)
+
+            return <>
+
+                <div>
+
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="300"
+                            img src={entry.fields.image.fields.file.url}
+                            alt="green iguana"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h3" component="div">
+                                {entry.fields.header}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {entry.fields.mainImageText}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Share</Button>
+                            <Button size="small">Learn More</Button>
+                        </CardActions>
+                    </Card>
+
+                </div>
+            </>
+        })}
+
+        </div>
+    );
+}
