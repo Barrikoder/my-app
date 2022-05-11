401a2de15a0fe4b1eb22338228f4b46dfb65fda3
anton litvak <anton.k.litvak@gmail.com>
2022-05-11 18:02:36 +0200

    removed type: module bbecause it was causing issues

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import AnotherDoner from "./Components/AnotherDoner";
 import jasonData from "./jasonData";
 
 // const contentful = require("contentful");
-// //const client = contentful.createClient({
+// const client = contentful.createClient({
 //   // This is the space ID. A space is like a project folder in Contentful terms
 //   space: process.env.REACT_APP_SPACE_ID,
 //   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
